1ef4892e7fb14c53418bb96dcfe162f649cbad87
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-05-10 08:50:21 +0700

    Update Profile.cs

--- ManagementStore/Form/User/Profile.cs
+++ ManagementStore/Form/User/Profile.cs
@@ -1,8 +1,12 @@
 ﻿using DevExpress.XtraEditors;
+using ManagementStore.Extensions;
+using ManagementStore.Model;
+using ManagementStore.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,9 +17,33 @@ namespace ManagementStore.Form.User
 {
     public partial class Profile : DevExpress.XtraEditors.XtraForm
     {
+        //SqlConnection connect = new SqlConnection(@"Data Source=DESKTOP-2EUHBHG\SQLEXPRESS;Initial Catalog=ComputerStore;Persist Security Info=True;User ID=sa;Password=Password@123"); //open connection to SQL Server
+        UserServices user = new UserServices();
+
         public Profile()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            AppUser appUser = new AppUser();
+            appUser.Firstname = txtFirstName.Text;
+            appUser.Lastname = txtLastName.Text;
+            appUser.Email = txtEmail.Text;
+            appUser.Address = txtAddress.Text;
+            appUser.Phone = txtPhone.Text;
+            appUser.Username = txtUsername.Text;
+            appUser.Password = PasswordExtensions.HashPassword(txtPassword.Text);
+            var result = user.RegisterUser(appUser,CurrentUser.AppUser.Id);
+            if(result.Success == true)
+            {
+                XtraMessageBox.Show(result.Message, "Changed Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                XtraMessageBox.Show(result.Message, "Changed Not Successful", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
